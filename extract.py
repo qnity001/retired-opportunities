555d3b9d7abc2756f5c32a1f405ee732bd53b827
Aarya Aishwaryam <aishwaryam.aarya@gmail.com>
2025-06-15 13:07:56 +0530

    Add code to apply OCR to entire PDF if garbage

--- extract.py
+++ extract.py
@@ -4,28 +4,30 @@ import re
 import sys
 
 # Searchable pdf
-def extract_plain(pdf):
+def extract(pdf):
     all_text = ""
     for page in pdf:
         text = page.get_text(sort = True)
         if check_garbage(text):
-            text = extract_OCR(pdf, page)
-        all_text += text + chr(12)
+            return extract_OCR(pdf)
+        all_text += text + "\n"
+    return all_text
 
 # Scanned text
-def extract_OCR(pdf, page):
-    for item in page.get_images():
-        xref = item[0]
-        pix = fitz.Pixmap(pdf, xref)
-        pdfdata = pix.pdfocr_tobytes()
-        ocrpdf = fitz.open("pdf", pdfdata)
-        ocrtext = ocrpdf[0].get_text(sort = True)
-        return ocrtext
+def extract_OCR(pdf):
+    all_text = ""
+    for page in pdf:
+        for item in page.get_images():
+            xref = item[0]
+            pix = fitz.Pixmap(pdf, xref)
+            pdfdata = pix.pdfocr_tobytes()
+            ocrpdf = fitz.open("pdf", pdfdata)
+            ocrtext = ocrpdf[0].get_text(sort = True)
+            all_text += ocrtext + "\n"
+    return all_text
 
 def check_garbage(text: str):
     if len(text) < 200:
-        return True
-    elif not text.isascii():
         return True
     elif check_alphanum(text) > 0.5:
         return True
@@ -42,5 +44,4 @@ def check_alphanum(text: str):
 
 if __name__ == "__main__":
     pdf = fitz.open(sys.argv[1])
-    all_text = extract_OCR(pdf)
-    print(all_text)
+    print(extract(pdf))
