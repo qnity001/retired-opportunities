74f2f8f84298b5d45d306ad7ddfe347cba07187c
Aarya Aishwaryam <aishwaryam.aarya@gmail.com>
2025-06-22 23:23:52 +0530

    Reject links that are not in the current year

--- scraper.py
+++ scraper.py
@@ -1,6 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
+from datetime import datetime
+
+current_year = str(datetime.today().year)
 
 def direct_links():
     links = []
@@ -9,5 +12,7 @@ def direct_links():
     soup = BeautifulSoup(response.text, "html.parser")
     for link in soup.select("a[href$='.pdf']"):
         full_link = urljoin(url, link['href'])
+        if current_year not in full_link:
+            continue
         links.append(full_link)
     return links
