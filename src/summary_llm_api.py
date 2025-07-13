e76586dd5e50bc373fa6892adf783c91d403da29
Aarya Aishwaryam <aishwaryam.aarya@gmail.com>
2025-07-13 17:32:04 +0530

    Add code to get summaries through deepseek LLM API

---
+++ src/summary_llm_api.py
@@ -0,0 +1,47 @@
+import re
+from openai import OpenAI
+
+with open("content.txt", "r",  encoding="utf-8", errors="ignore") as file:
+    content = file.read()
+    content = re.sub(r'\n\s*\n+', '\n\n', content)
+    content = re.sub(r'[ \t]+', ' ', content) 
+
+def summarize(content: str):
+    client = OpenAI(api_key="sk-d2484100bf1444b6a8ee278dcf9ed69b", base_url="https://api.deepseek.com")
+
+    input_prompt = f"""
+    You are a government job summarizer. Summarize the key points from this PDF recruitment notice for retired professionals in under 150 words. 
+    
+    Instructions:
+    - Read the content carefully.
+    - Highlight only the useful information like skills, post name and job duration. 
+    - Exclude instructions, forms and annexures, age limit, pay scale and application deadline. 
+    - Summarize the following job posting strictly in a single, well-structured paragraph. Do not use bullet points or lists. Keep the summary concise, clear, and human-readable. 
+    - Output ONLY the summary as plain text. Do NOT add introductions, disclaimers, or extra sentences. Begin directly with the first word of the summary.
+    
+    Text: {content} 
+    Summary: 
+    """
+
+    response = client.chat.completions.create(
+        model="deepseek-chat",
+        messages=[
+            {"role": "user", "content": input_prompt},
+        ],
+        stream=False
+    )
+    return response.choices[0].message.content
+
+'''
+def get_metadata(content: str):
+
+    input_prompt = f"You are a government job assistant who checks for recruitments. The job post might be poorly formatted or noisy or garbled or jumbled. Extract the following fields from this government job post and return them as a JSON object: 'job_title', 'eligibility', 'minimum_qualification', 'age_limit', 'application_deadline', 'pay_scale', 'employment_type', 'field_of_work'. If any field is not present, return 'unknown'. Text: {content}"
+
+    response = ollama.generate(
+        model = "mistral:7b-instruct",
+        prompt = input_prompt
+    )
+    json_object = response["response"]
+    metadata = json.loads(json_object)
+    return metadata
+    '''
